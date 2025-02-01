cd083aa99d9033363f265c08dee5ac1214d1ad34
okayenu <okechukwu.ndubuisi@icloud.com>
2025-02-01 13:27:05 -0600

    Make Csrt Quantity Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -89,6 +89,14 @@ products.forEach((product)=>
             )
           }
 
+          let cartQuantity=0;
+          cart.forEach((item)=>
+          {
+           cartQuantity+=item.quantity; 
+          })
+
+          document.querySelector('.js-cart-quantity').innerHTML= cartQuantity;
+
           console.log(cart);
 
         })
