ba5b4890ec9f2b95f17c09825238567f94713d0d
okayenu <okechukwu.ndubuisi@icloud.com>
2025-04-16 16:15:00 -0500

    Write tests for recommendation determinism in community experiences give

--- scripts/seed-general.js
+++ scripts/seed-general.js
@@ -1,6 +1,6 @@
 /**
  * Seed-data generator: general
- * Task: Write tests for recommendation determinism in merchant operations given fixed se
+ * Task: Write tests for recommendation determinism in community experiences given fixed 
  *
  * Usage: node scripts/seed-general.js
  */
