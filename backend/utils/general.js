ba6114695a5dde5cd55ec0616b7b9a0bb4a62517
okayenu <okechukwu.ndubuisi@icloud.com>
2025-05-05 20:15:00 -0500

    Add fallback heuristics for cold-start users in recommendation quality:

--- backend/utils/general.js
+++ backend/utils/general.js
@@ -1,6 +1,6 @@
 /**
  * General
- * Task: Create recommendation candidate generator for 3D product rendering combining col
+ * Task: Add fallback heuristics for cold-start users in recommendation quality: popular-
  */
 'use strict';
 
