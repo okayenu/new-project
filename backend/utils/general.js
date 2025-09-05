b662ae602bf7973466189487cb2ef71cfbed319b
okayenu <okechukwu.ndubuisi@icloud.com>
2025-09-05 05:00:00 -0500

    Design 3D asset contract for checkout orchestration (GLTF variants, text

--- backend/utils/general.js
+++ backend/utils/general.js
@@ -1,6 +1,6 @@
 /**
  * General
- * Task: Document 3D authoring guidelines for catalog intelligence contributors and QA ac
+ * Task: Design 3D asset contract for checkout orchestration (GLTF variants, texture rule
  */
 'use strict';
 
