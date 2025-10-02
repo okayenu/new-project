4f64b5ddaee434e0d1c73b4bd2e9ff53f63632fb
okayenu <okechukwu.ndubuisi@icloud.com>
2025-10-02 17:45:00 -0500

    Write tests for personalization race conditions (inventory contention, d

--- backend/utils/general.js
+++ backend/utils/general.js
@@ -1,6 +1,6 @@
 /**
  * General
- * Task: Implement risk checks for 3D product rendering one-click orders (velocity limits
+ * Task: Write tests for personalization race conditions (inventory contention, duplicate
  */
 'use strict';
 
