c6bec9aed925f5a3c5b4d6f0eb652e82199184a1
okayenu <okechukwu.ndubuisi@icloud.com>
2025-11-21 01:00:00 -0600

    Create social-interest graph schema for recommendation quality linking u

--- backend/utils/general.js
+++ backend/utils/general.js
@@ -1,6 +1,6 @@
 /**
  * General
- * Task: Create admin controls for search and discovery community governance, escalation 
+ * Task: Create social-interest graph schema for recommendation quality linking users, ta
  */
 'use strict';
 
